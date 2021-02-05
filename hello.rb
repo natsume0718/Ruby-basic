cb2a508adf91b93bce69ad4bb8dad0ee524e1d9a
natsume0718 <moto718program@gmail.com>
2021-02-06 00:22:00 +0900

    update

--- hello.rb
+++ hello.rb
@@ -2,10 +2,20 @@
 hello = 'hello world'
 
 # 関数定義
-def putsHello()
+def putHello()
   puts 'hello method'
+end
+
+# デフォルト引数も可能
+def sum(val, val2 = 1)
+  # メソッドでは最後に評価した式が戻り値になるので、returnを明示しなくて良い
+  # 明示した場合はその時点で戻る
+  val + val2
 end
 
 puts hello
 
-putsHello
+putHello
+
+# 三項演算子もつかえる
+puts 10 < 20 ? '20がおおきい' : '10がおおきい'
