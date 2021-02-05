f4056889b3a973ce2fa187b95c19be26aa9a8c90
natsume0718 <moto718program@gmail.com>
2021-02-06 00:59:33 +0900

    add class

---
+++ user.rb
@@ -0,0 +1,55 @@
+class User
+  # クラスプロパティ(static プロパティにあたる?)
+  @@name = 'static的な'
+  # 定数
+  GREETING_MSG = 'hello'
+
+  def initialize
+    # PHPとかでいうプロパティに当たる
+    @id = 1
+  end
+
+  def updateName
+    @@name = 'アップデートした'
+  end
+
+  def getId
+    @id
+  end
+
+  def putsUserClassInfo
+    # 疑似変数
+    puts __FILE__
+    puts __LINE__
+  end
+
+  def getName
+    # メソッドでは最後に評価した式が戻り値になるので、returnを明示しなくて良い
+    # 明示した場合はその時点で戻る
+    @@name
+  end
+
+  # クラスメソッド => staticメソッド
+  def self.hoge
+    puts 'hoge'
+  end
+
+end
+
+user = User.new
+user2 = User.new
+
+puts user.getId
+puts user.getName
+user.updateName
+puts user.getName
+puts user2.getName
+
+User.hoge
+
+
+__END__
+これ以降は
+全ての記述が
+
+コメントとなる
